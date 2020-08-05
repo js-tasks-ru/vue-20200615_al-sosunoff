be6f32dc303b4b7dad12ef596b23d81c38da8122
asosunoff <al.sosunoff@gmail.com>
2020-08-05 23:21:43 +0300

    fix

--- 4-module/4-task/data.js
+++ 4-module/4-task/data.js
@@ -1,3 +1,4 @@
+import fetchJson from '../../utils/fetch-json.js';
 /** URL адрес API */
 export const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -7,7 +8,29 @@ export const API_URL = 'https://course-vue.javascript.ru/api';
  * @param {string} password - пароль пользователя
  * @return {Promise} - объект с данными пользователя
  */
-export async function login(email, password) {}
+export async function login(email, password) {
+  try {
+    return await fetchJson(`${API_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+  } catch (error) {
+    switch (error.body.statusCode) {
+      case 401:
+        throw new Error('Вы не авторизованы');
+      case 403:
+        throw new Error(error.body.message);
+      default:
+        throw new Error('Не известная ошибка');
+    }
+  }
+}
 
 /**
  * Выполняет авторизацию по логину и паролю и возвращает результат
@@ -16,4 +39,27 @@ export async function login(email, password) {}
  * @param {string} password - пароль пользователя
  * @return {Promise} - объект с данными зарегистрированного пользователя
  */
-export async function register(email, fullname, password) {}
+export async function register(email, fullname, password) {
+  try {
+    return await fetchJson(`${API_URL}/auth/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        email,
+        fullname,
+        password,
+      }),
+    });
+  } catch (error) {
+    switch (error.body.statusCode) {
+      case 401:
+        throw new Error('Вы не авторизованы');
+      case 403:
+        throw new Error(error.body.message);
+      default:
+        throw new Error('Не известная ошибка');
+    }
+  }
+}
--- 4-module/4-task/views/LoginPage.vue
+++ 4-module/4-task/views/LoginPage.vue
@@ -1,12 +1,75 @@
 <template>
-  <form class="form"></form>
+  <form class="form" @submit.prevent="submitHandler">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input
+          v-model="email"
+          type="email"
+          placeholder="demo@email"
+          class="form-control"
+        />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input
+          v-model="password"
+          type="password"
+          placeholder="password"
+          class="form-control"
+        />
+      </div>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary button_block">
+        Войти
+      </button>
+    </div>
+    <div class="form__append">
+      Нет аккаунта?
+      <router-link to="/register" class="link">Зарегистрируйтесь</router-link>
+    </div>
+  </form>
 </template>
 
 <script>
-// import { login } from '../data';
+import { login } from '../data';
 
 export default {
   name: 'LoginPage',
+  data: () => ({
+    email: '',
+    password: '',
+    errorMessage: '',
+  }),
+  methods: {
+    validate() {
+      if (this.email.length === 0) {
+        this.errorMessage = 'Требуется ввести Email';
+        return false;
+      }
+
+      if (this.password.length === 0) {
+        this.errorMessage = 'Требуется ввести пароль';
+        return false;
+      }
+
+      return true;
+    },
+    async submitHandler() {
+      try {
+        if (!this.validate()) {
+          throw new Error(this.errorMessage);
+        }
+        const { fullname } = await login(this.email, this.password);
+        alert(fullname);
+      } catch (error) {
+        alert(error.message);
+      }
+    },
+  },
 };
 </script>
 
