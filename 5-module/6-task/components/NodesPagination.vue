ffd01546e2e1db8a861b6671aa1a33875e607190
asosunoff <al.sosunoff@gmail.com>
2020-08-15 18:34:04 +0300

    fix

--- 5-module/6-task/components/NodesPagination.vue
+++ 5-module/6-task/components/NodesPagination.vue
@@ -1,6 +1,26 @@
 <script>
 export default {
   name: 'NodesPagination',
+  inheritAttrs: false,
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+    perPage: {
+      type: Number,
+      default: 5,
+    },
+  },
+  render(c) {
+    const start = (this.page - 1) * this.perPage;
+    const count = start + this.perPage;
+
+    const items = this.$slots.default
+      ? this.$slots.default.slice(start, count)
+      : this.$slots.default;
+    return c('div', items);
+  },
 };
 </script>
 
