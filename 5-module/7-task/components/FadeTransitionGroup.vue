2f9711f38caacb2716df217c34d6a75b27330fd6
asosunoff <al.sosunoff@gmail.com>
2020-08-15 22:20:00 +0300

    fix

--- 5-module/7-task/components/FadeTransitionGroup.vue
+++ 5-module/7-task/components/FadeTransitionGroup.vue
@@ -1,7 +1,55 @@
 <script>
 export default {
   name: 'FadeTransitionGroup',
+  inheritAttrs: false,
+  render(c) {
+    if (this.$slots.default) {
+      this.$slots.default.forEach((vnode) => {
+        vnode.data = vnode.data || {};
+        vnode.data.class = { ...vnode.data.class, 'fade-list-item': true };
+      });
+    }
+
+    return c(
+      'transition-group',
+      {
+        attrs: {
+          ...this.$attrs,
+        },
+        props: {
+          name: 'fade-list',
+        },
+        on: {
+          ...this.$listeners,
+        },
+      },
+      this.$slots.default,
+    );
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+/deep/ .fade-list {
+  position: relative;
+}
+
+.fade-list /deep/ .fade-list-item {
+  transition: all 0.3s ease-out;
+}
+
+/deep/ .fade-list-leave-active {
+  position: absolute !important;
+  left: 0;
+  right: 0;
+}
+
+/deep/ .fade-list-enter,
+/deep/ .fade-list-leave-to {
+  opacity: 0;
+}
+
+/deep/ .fade-list-move {
+  transition: transform 0.3s;
+}
+</style>
